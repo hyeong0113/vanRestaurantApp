0943f8631d3524b94db9fe91a29a781b545e96a6
hyeong0113 <kwakdent95@gmail.com>
2023-03-01 16:01:11 -0800

    Top restaurant loaded to frontend

--- vanRestaurantFrontend/src/components/GeoLocation.js
+++ vanRestaurantFrontend/src/components/GeoLocation.js
@@ -14,41 +14,75 @@ const requestOptions = {
 };
 
 function GeoLocation() {
-    const [data, setData] = useState(null);
-    const [restaurants, setRestaurants] = useState([]);
+    const [geoData, setGeoData] = useState(null);
+    const [restaurants, setRestaurants] = useState(null);
+    const [topRestaurant, settopRestaurant] = useState(null);
+
     const [isDataLoading, setIsDataLoading] = useState(true);
     const [isRestaurantsLoading, setIsRestaurantsLoading] = useState(true);
+    const [isTopRestaurantLoading, setisTopRestaurantLoading] = useState(true);
 
+    // Fetch geolocation data
     useEffect(() => {
-        if(isDataLoading) {
-            fetch(`http://127.0.0.1:8080/geo`, requestOptions)
-            .then(res => res.json())
-            .then(
-            (result) => {
-                console.log("data loaded");
-                setData(result);
-                setIsDataLoading(false);
-            },
-            (error) => {
-                console.log("Not loaded");
+        const fetchGeoData = async() => {
+            if(isDataLoading) {
+                await fetch(`http://127.0.0.1:8080/geo`, requestOptions)
+                    .then(res => res.json())
+                    .then(
+                        (result) => {
+                            console.log("data loaded");
+                            setGeoData(result);
+                            setIsDataLoading(false);
+                        },
+                        (error) => {
+                            console.log("Not loaded");
+                        }
+                    )
             }
-            )
         }
-    },[])
+        fetchGeoData();
+    }, [])
 
+    // Fetch restaurants list
     useEffect(() => {
         const fetchRestaurants = async() => {
-            if(data == null) {
+            if(geoData == null) {
                 return;
             }
             if(isRestaurantsLoading) {
-                await fetch(`http://127.0.0.1:8080/restaurants/lat/${data.location.lat}/long/${data.location.lng}`, requestOptions)
+                await fetch(`http://127.0.0.1:8080/restaurants/lat/${geoData.location.lat}/long/${geoData.location.lng}`, requestOptions)
+                    .then(res => res.json())
+                    .then(
+                        (result) => {
+                            console.log("restaurants loaded");
+                            setRestaurants(result);
+                            setIsRestaurantsLoading(false);
+                        },
+                        (error) => {
+                            console.log("Not loaded");
+                        }
+                    )
+            }
+        }
+        fetchRestaurants();
+    }, [geoData])
+
+    // Fetch top restaurant
+    useEffect(() => {
+        const fetchTopRestaurant = async() => {
+            if(restaurants == null) {
+                return;
+            }
+            if(isTopRestaurantLoading) {
+                console.log("isTopRestaurantLoading: " + isTopRestaurantLoading);
+
+                await fetch(`http://127.0.0.1:8080/restaurant/top/${restaurants.topId}`, requestOptions)
                 .then(res => res.json())
                 .then(
                     (result) => {
-                        console.log("restaurants loaded");
-                        setRestaurants(result);
-                        setIsRestaurantsLoading(false);
+                        console.log("top restaurant loaded");
+                        settopRestaurant(result);
+                        setisTopRestaurantLoading(false);
                     },
                     (error) => {
                         console.log("Not loaded");
@@ -56,25 +90,29 @@ function GeoLocation() {
                 )
             }
         }
-        fetchRestaurants();
-    }, data)
+        fetchTopRestaurant();
+    }, [restaurants])    
 
-    if (!data || !restaurants) {
+    if (!geoData || !restaurants || !topRestaurant) {
         return <div>Loading...</div>;
     }
 
     return (
         <div>
-            {/* {restaurants !== undefined && restaurants !== null ? 
-                console.log(restaurants.results)
+            {/* {restaurants.results !== undefined && restaurants.results !== null ? 
+                <RestaurantCard restaurant={restaurants.results[0]} />
                 : null
-            }             */}
-            {restaurants.results !== undefined && restaurants.results !== null ? 
+            } */}
+            {restaurants !== null ? 
                 <RestaurantCard restaurant={restaurants.results[0]} />
                 : null
             }
+            {topRestaurant !== null ?
+
+                console.log(topRestaurant) : null
+            }
         </div>
     );
-    }
+}
 
 export default GeoLocation;
