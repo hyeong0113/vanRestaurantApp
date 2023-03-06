4af58285316ee5ddf1e40ddb2e59a89bb35169e5
hyeong0113 <kwakdent95@gmail.com>
2023-03-05 16:38:00 -0800

    Google Map init

--- vanRestaurantFrontend/src/components/GoogleMapComponent.js
+++ vanRestaurantFrontend/src/components/GoogleMapComponent.js
@@ -1,47 +1,41 @@
-import { useState, useCallback } from 'react';
+import { useRef, useCallback } from 'react';
 import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
 
 const GoogleMapComponent = (props) => {
-    // start google map
-    const { isLoaded } = useJsApiLoader({
+    const { isLoaded, loadError } = useJsApiLoader({
         id: 'google-map-script',
         googleMapsApiKey: "AIzaSyAt3MM77NlV_PDgfy_CA4SYmc65-sBOCK8"
     })
 
-    const [map, setMap] = useState(null);
-    const [zoom, setZoom] = useState(20);
+    const mapRef = useRef();
+    const onMapLoad = useCallback((map) => {
+      mapRef.current = map;
+    }, []);
 
-    const onLoad = useCallback(function callback(map) {
-      // This is just an example of getting and using the map instance!!! don't just blindly copy!
-      const bounds = new window.google.maps.LatLngBounds(props.geoData);
-      map.fitBounds(bounds);
-  
-      setMap(map)
-    }, [])
-  
-    const onUnmount = useCallback(function callback(map) {
-      setMap(null)
-    }, [])  
-    // end google map
+    if (loadError) return "Error";
+    if (!isLoaded) return "Loading...";
 
-    const containerStyle = {
-        width: '800px',
-        height: '800px'
-      };
+    const mapContainerStyle = {
+        height: "60vh",
+        width: "900px"
+    };
+
+    const options = {
+        disableDefaultUI: true,
+        zoomControl: true
+    };
   
     return (
         <div>
-            {console.log(zoom)}
             {isLoaded &&
                 <GoogleMap
-                    mapContainerStyle={containerStyle}
-                    center={{ lat: props.geoData.lat, lng: props.geoData.lng, zoom: zoom }}
-                    defaultZoom={zoom}
-                    onLoad={onLoad}
-                    onUnmount={onUnmount}
+                    mapContainerStyle={mapContainerStyle}
+                    center={{ lat: props.geoData.lat, lng: props.geoData.lng }}
+                    zoom={14}
+                    onLoad={onMapLoad}
+                    options={options}
                 >
                 <Marker position={props.geoData} />
-                <></>
             </GoogleMap>
             }
         </div>
