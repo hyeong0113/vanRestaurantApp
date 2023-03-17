b53d7e8767064e8acbd0d0db303ba8e4ff0a0644
hyeong0113 <kwakdent95@gmail.com>
2023-03-17 14:30:14 -0700

    Top bar refactored

--- vanRestaurantFrontend/src/components/Topbar.js
+++ vanRestaurantFrontend/src/components/Topbar.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
+import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import { makeStyles } from '@mui/styles';
 import food1 from '../assets/images/food1.png';
@@ -8,30 +9,43 @@ import food2 from '../assets/images/food2.png';
 import food3 from '../assets/images/food3.png';
 import food4 from '../assets/images/food4.png';
 import food5 from '../assets/images/food5.png';
+import { Hidden, Typography } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+import PersonIcon from '@mui/icons-material/Person';
 
 const images = [food1, food2, food3, food4, food5];
 
 const useStyles = makeStyles((theme) => ({
-  appBar: {
-    backgroundColor: 'rgba(103, 69, 18, 0.46) !important',
+  background: {
+    backgroundColor: 'rgba(103, 69, 18, 0.46)',
     position: 'absolute',
+    overflow: 'hidden',
     height: 400,
   },
-  // title: {
-  //   flexGrow: 1,
-  //   margin: theme.spacing(2),
-  // },
+  title: {
+    width: '190px',
+    height: '80px',
+    paddingRight: '80%',
+    paddingTop: '30px',
+    fontWeight: '600 !important',
+    fontStyle: 'normal',
+    color: "#ffffff"
+  },
   toolbar: {
     display: 'flex',
     justifyContent: 'space-between',
     padding: theme.spacing(0, 1),
-    ...theme.mixins.toolbar,
-  },  
+  },
+  icon: {
+    height: '40px !important',
+    width: '40px !important',
+    color: "#ffffff"
+  },
   image: {
     position: 'absolute',
-    width: 370,
-    height:370,
-    bottom: 0,
+    width: 400,
+    height:400,
+    paddingTop: 100,
     right: 0
   },
 }));
@@ -51,16 +65,29 @@ const Topbar = () => {
   const backgroundImage = `${images[currentImage]}`;
 
   return (
-    <AppBar className={classes.appBar}>
-      <Toolbar className={classes.appBar}>
+    <Grid className={classes.background} container>
+      <Grid item xs={6}>
+        <Typography className={classes.title} variant='h2'>
+          NearBy
+        </Typography>
+      </Grid>
+      <Grid item xs={6}>
+        <Grid container>
+          <Grid item xs={6}>
+            <HomeIcon className={classes.icon} />
+          </Grid>
+          <Grid item xs={6}>
+            <PersonIcon className={classes.icon} />
+          </Grid>
+        </Grid>
         <Box
-            component="img"
-            className={classes.image}
-            alt="Your logo."
-            src={backgroundImage}
-        />
-      </Toolbar>
-    </AppBar>
+           component="img"
+           className={classes.image}
+           alt="food image"
+           src={backgroundImage}
+        /> 
+      </Grid>
+    </Grid>
   );
 }
 
--- vanRestaurantFrontend/src/theme/customTheme.js
+++ vanRestaurantFrontend/src/theme/customTheme.js
@@ -1,25 +1,25 @@
 import { createTheme } from '@mui/material/styles';
 
 const customTheme = createTheme({
-  // typography: {
-  //   h6: {
-  //     fontFamily: 'Noto sans, sans-serif;',
-  //   },
-  //   body4: {
-  //     fontFamily: 'Noto sans, sans-serif;',
-  //   }
-  // },
-  // palette: {
-  //   primary: {
-  //     main: '#FF9F46',
-  //   },
-  //   secondary: {
-  //     main: '#FFB800',
-  //   },
-  //   info: {
-  //     main: '#FFF2D0'
-  //   }
-  // }
+  typography: {
+    h2: {
+      fontFamily: 'Manrope, sans-serif',
+    },
+    // body4: {
+    //   fontFamily: 'Noto sans, sans-serif;',
+    // }
+  },
+  palette: {
+    primary: {
+      main: 'rgba(103, 69, 18, 0.46)',
+    },
+    // secondary: {
+    //   main: '#FFB800',
+    // },
+    // info: {
+    //   main: '#FFF2D0'
+    // }
+  }
 });
 
 export default customTheme;
