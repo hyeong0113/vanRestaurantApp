8a4bf174bdd8e11fbf799cf78f77494010cf1924
hyeong0113 <kwakdent95@gmail.com>
2023-04-07 00:09:11 -0700

    Update favoriteRestaurantUtility.js
    - createFavoriteRestaurant created

--- vanRestaurantBackend/utilities/favoriteRestaurantUtility.js
+++ vanRestaurantBackend/utilities/favoriteRestaurantUtility.js
@@ -5,20 +5,30 @@ require('dotenv').config();
 
 
 const saveFavoriteRestaurant = async (favoriteRestaurant, user) => {
-    const convertedFavoriteRestaurant = convertToFavoriteRestaurant(favoriteRestaurant);
-    
-    convertedFavoriteRestaurant.userId = user._id;
+    let found = await user.favoriteRestaurants.find(r => r.placeId === favoriteRestaurant.placeId);
+    if(!found) {
+        const convertedFavoriteRestaurant = convertToFavoriteRestaurant(favoriteRestaurant);
+        
+        convertedFavoriteRestaurant.userId = user._id;
 
-    convertedFavoriteRestaurant.save(function(err) {
-        if (err) throw err;
-        console.log('Favorite restaurant saved!');
-    });
+        convertedFavoriteRestaurant.save(function(err) {
+            if(err) {
+                return { message: err, success: false }
+            }
+            console.log('Favorite restaurant saved!');
+            return { message: "Favorite restaurant saved!", success: true }
+        });
 
-    user.favoriteRestaurants.push(convertedFavoriteRestaurant);
-    user.save(function(err) {
-        if (err) throw err;
-        console.log('Favorite restaurant save to user!');
-    });
+        user.favoriteRestaurants.push(convertedFavoriteRestaurant);
+        user.save(function(err) {
+            if(err) {
+                return { message: err, success: false }
+            }
+            console.log('Favorite restaurant save to user!');
+            return { message: "Favorite restaurant save to user!", success: true }
+        });
+    }
+    return { message: "Favorite restaurant exist!", success: true }
 }
 
 module.exports = {
