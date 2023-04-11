8848f398e9f02334057fe161715960ee713a1998
hyeong0113 <kwakdent95@gmail.com>
2023-04-11 15:36:12 -0700

    Fixed logic to reduce duplicative parts

--- vanRestaurantBackend/controllers/favoriteRestaurantController.js
+++ vanRestaurantBackend/controllers/favoriteRestaurantController.js
@@ -1,81 +1,76 @@
 const jwt = require("jsonwebtoken");
 const User = require('../models/userSchema');
 const FavoriteRestaurant = require('../models/favoriteRestaurantSchema');
-const { saveFavoriteRestaurant } = require('../utilities/favoriteRestaurantUtility');
+const { saveFavoriteRestaurant, populateFavoriteRestaurants } = require('../utilities/favoriteRestaurantUtility');
 require('dotenv').config();
 
 const getFavoriteRestaurantsByUserId = async(req, res) => {
     const { user } = req;
-
-    try {
-        var populatedUser = await user.populate('favoriteRestaurants');
+    if(user.isLoggedIn) {
+        const populatedUser = await populateFavoriteRestaurants(user);
+        return res.status(200).json(populatedUser);
     }
-    catch(err) {
-        throw res.status(500).json(error);
+    else {
+        return res.status(401).json({ message: "Unauthorized action. Please log in first.", success: false });
     }
-    return res.status(200).json(populatedUser);
 }
 
 const createFavoriteRestaurant = async (req, res) => {
     const { user } = req;
-    const { favoriteRestaurant } = req.body;
-    try {
-        var populatedUser = await user.populate('favoriteRestaurants');
+    if(user.isLoggedIn) {
+        const populatedUser = await populateFavoriteRestaurants(user);
+        const { favoriteRestaurant } = req.body;
+        const response = await saveFavoriteRestaurant(favoriteRestaurant, populatedUser);
+        return res.status(200).json(response);
     }
-    catch(err) {
-        throw res.status(500).json(error);
+    else {
+        return res.status(401).json({ message: "Unauthorized action. Please log in first.", success: false });
     }
-
-    const response = await saveFavoriteRestaurant(favoriteRestaurant, populatedUser);
-    return res.status(200).json(response);
 }
+    
 
 const deleteFavoriteRestaurantByPlaceId = async (req, res) => {
-    const { placeId } = req.body;
     const { user } = req;
+    if(user.isLoggedIn) {
+        const populatedUser = await populateFavoriteRestaurants(user);
+        const { placeId } = req.body;
 
-    try {
-        var populatedUser = await user.populate('favoriteRestaurants');
+        const deleteFavoriteRestaurant = populatedUser.favoriteRestaurants.find(r => r.placeId === placeId);
+    
+        if(deleteFavoriteRestaurant) {
+            await FavoriteRestaurant.findOneAndRemove({ _id: deleteFavoriteRestaurant._id, userId: user._id });
+            user.favoriteRestaurants.pull(deleteFavoriteRestaurant);
+            await populatedUser.save();
+            console.log('Favorite restaurant deleted successfully.');
+            res.status(200).json({ message: "Favorite restaurant deleted", success: true });
+        } else {
+            console.log('Favorite restaurant not found.');
+            res.status(200).json({ message: "Favorite restaurant not found", success: false });
+        }
     }
-    catch(err) {
-        throw res.status(500).json(error);
-    }
-
-    const deleteFavoriteRestaurant = populatedUser.favoriteRestaurants.find(r => r.placeId === placeId);
-
-    if(deleteFavoriteRestaurant) {
-        await FavoriteRestaurant.findOneAndRemove({ _id: deleteFavoriteRestaurant._id, userId: user._id });
-        user.favoriteRestaurants.pull(deleteFavoriteRestaurant);
-        await populatedUser.save();
-        console.log('Favorite restaurant deleted successfully.');
-        res.status(200).json({ message: "Favorite restaurant deleted", success: true });
-    } else {
-        console.log('Favorite restaurant not found.');
-        res.status(200).json({ message: "Favorite restaurant not found", success: false });
+    else {
+        return res.status(401).json({ message: "Unauthorized action. Please log in first.", success: false });
     }
 }
 
 const deleteAllFavoriteRestaurants = async (req, res) => {
     const { user } = req;
-
-    try {
-        var populatedUser = await user.populate('favoriteRestaurants');
+    if(user.isLoggedIn) {
+        const populatedUser = await populateFavoriteRestaurants(user);
+        try {
+            await FavoriteRestaurant.deleteMany({ userId: populatedUser._id });
+            populatedUser.favoriteRestaurants = [];
+            await populatedUser.save();
+            console.log('Favorite restaurant deleted successfully.');
+            res.status(200).json({ message: "All favorite restaurants deleted", success: true });
+        }
+        catch(err) {
+            res.status(403).json({ message: err, success: false });
+        }
     }
-    catch(err) {
-        throw res.status(500).json(error);
+    else {
+        return res.status(401).json({ message: "Unauthorized action. Please log in first.", success: false });
     }
-    
-    try {
-        await FavoriteRestaurant.deleteMany({ userId: populatedUser._id });
-        populatedUser.favoriteRestaurants = [];
-        await populatedUser.save();
-        console.log('Favorite restaurant deleted successfully.');
-        res.status(200).json({ message: "All favorite restaurants deleted", success: true });
-    }
-    catch(err) {
-        res.status(403).json({ message: err, success: false });
-    }
-
 }
 
 module.exports = {
