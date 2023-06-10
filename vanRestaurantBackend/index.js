4b5fa2f2b5d2f7907f64f5cf2d51a1b0c6e4b7a3
hyeong0113 <kwakdent95@gmail.com>
2023-06-10 00:59:34 -0700

    Update index.js

--- vanRestaurantBackend/index.js
+++ vanRestaurantBackend/index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: "http://localhost:5000"
+  origin: "http://localhost:3000"
 }));
 
 const userName = process.env.USERNAME;
