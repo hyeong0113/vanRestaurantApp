527c896b3bd3a89d5838dfda623dd852d2b0554b
hyeong0113 <kwakdent95@gmail.com>
2023-06-19 21:00:06 -0700

    Update url

--- vanRestaurantBackend/index.js
+++ vanRestaurantBackend/index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: "http://ec2-54-215-91-235.us-west-1.compute.amazonaws.com:3000"
+  origin: "http://ec2-13-56-156-127.us-west-1.compute.amazonaws.com:3000"
 }));
 
 const userName = process.env.USERNAME;
