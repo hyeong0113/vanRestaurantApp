8406df375e01e319794948817bc363412f61cb25
hyeong0113 <kwakdent95@gmail.com>
2023-06-21 10:44:39 -0700

    Edit dockerfile

--- vanRestaurantBackend/index.js
+++ vanRestaurantBackend/index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: "http://ec2-13-56-156-127.us-west-1.compute.amazonaws.com:3000"
+  origin: "http://ec2-54-67-27-195.us-west-1.compute.amazonaws.com:3000"
 }));
 
 const userName = process.env.USERNAME;
